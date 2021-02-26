430f957b11aed9f9030fc2802532f8f7a5c8c386
Delower Hossen <delowerus@gmail.com>
2021-02-26 02:09:51 -0500

    Creating SearchPage

---
+++ src/test/java/framework/webpages/SearchPage.java
@@ -0,0 +1,18 @@
+package framework.webpages;
+
+import org.openqa.selenium.By;
+
+public class SearchPage extends BasePage{
+
+    private By postalCode = By.xpath("//input[@id='Postal_Code_Sep']");
+    private By searchButton = By.xpath("//button[@type='submit']");
+
+    public void enterPostalCode(String code){
+        setValue(postalCode, code);
+    }
+
+    public void iClickOnSearchButton(){
+        clickOnWebElement(searchButton);
+    }
+
+}
